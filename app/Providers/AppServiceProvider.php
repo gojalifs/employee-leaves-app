dcb94184118b00cd5786455a28a1af0fb3f6310f
Fajar Sidik <Fajarsidik1999@gmail.com>
2025-03-08 22:47:26 +0700

    feat: Grant "Super Admin" role all permissions using Gate functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Enums\RoleEnum;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        // Implicitly grant "Super Admin" role all permissions
+        // This works in the app by using gate-related functions like auth()->user->can() and @can()
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole(RoleEnum::SUPER_ADMIN) ? true : null;
+        });
     }
 }
